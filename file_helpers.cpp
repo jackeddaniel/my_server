be4a75d2c148331cc55adc4d8e7e4ec604b0de98
jackeddaniel <siddhant.ugarkar@gmail.com>
2026-06-05 13:38:22 +0530

    fixed the insecure path issue

--- file_helpers.cpp
+++ file_helpers.cpp
@@ -22,8 +22,32 @@ string html_to_string(const string& path) {
     return file_contents;
 }
 
+int valid_path(const string& req_path) {
+    int stack_top = 0;
+    if(req_path.empty() || req_path[0] != '/') return 0;
+    if(req_path.size() > 2 && req_path[1] == '.' && req_path[2] == '.') return 0;
+
+    int i = 0;
+
+    while(i < req_path.size()) {
+        while(i < req_path.size() - 1 && req_path[i] != '/') i++;
+        if(i >= req_path.size()) break;
+
+        if(i+2 < req_path.size() && (req_path[i+1] == '.' && req_path[i+2] == '.')) {
+            stack_top--;
+            if(stack_top < 0) {
+                return 0;
+            }
+        } else {
+            stack_top++;
+        }
+        i++;
+    }
+    return 1;
+}
 
 int path_builder(const string& req_path, string& path) {
+    if(!valid_path(req_path)) return 0;
     string parent = "static";
     filesystem::path combined_path = parent + req_path;
     cout<<combined_path<<endl;
@@ -43,13 +67,14 @@ void test_func() {
 }
 /*
 int main() {
-    string path = "/index.html";
-    string op_path;
-    if(path_builder(path,op_path) == 0) {
-        cout<<"error"<<endl;
-    }
 
-    cout<<op_path<<endl;
+    int res;
+
+    string path = "../catamaran/../../vo/../../xoxo/../xoox";
+    res = valid_path(path);
+
+    if(res) cout<<"Path valid"<<endl;
+    if(!res) cout<<"Path invalid"<<endl;
     
 }
 */
